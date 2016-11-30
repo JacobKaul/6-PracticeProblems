3ed1912d11c1733b96d85aac686c746bdae75611
Jacob H. Kaul <JacobKaul@jacobs-mbp.cvtc.local>
2016-11-30 11:50:55 -0600

    initial push

---
+++ JS/index.js
@@ -0,0 +1,53 @@
+// #1
+// while
+document.write("1.");
+document.write("<br>");
+  var i = 0;
+
+  while (i >= 1, i <= 100)
+  {
+    document.write(i);
+    i++;
+  }
+  document.write("<br>");
+  
+    var i = 0;
+// do while
+  do
+  {
+    document.write(i);
+    i++;
+  }
+  while (i >= 1, i <= 100)
+
+    document.write("<br>");
+  //for
+    for(var i = 0; i >= 1, i <= 100; i++);
+  {
+    document.write(i);
+  }
+
+// #2
+document.write("<br>");
+document.write("2.");
+document.write("<br>");
+
+function fizzBuzz() {
+  for (var isOdd() === 1; isOdd() >= 100; i++) {
+    if (isOdd() === 1) {
+      document.write("fizz"); 
+    }
+  }
+}
+
+function isOdd() {
+  return i % 2 == 1;
+}
+}
+fizzBuzz();
+
+// 3
+    document.write("<br>");
+document.write("3.");
+    document.write("<br>");
+
